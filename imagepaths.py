f22ac3fd3bb8136f063527c4c69ee04ed56ee66b
Rwon01 <152537958+Rwon01@users.noreply.github.com>
2023-12-05 15:35:54 -0500

    Start over

--- imagepaths.py
+++
@@ -1,5 +0,0 @@
-
-player_path = "assets/player.png"
-enemy_path = "assets/player.png" # using default player for enemies for now
-bullet_path = "assets/bullet.png"
-background_path = "assets/background.jpg"
